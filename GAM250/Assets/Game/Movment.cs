4ac9042f39f94cf7363c27dfd6b42ed54cdbabcd
ArchieAndrewsFal <archieandrews@live.co.uk>
2017-11-29 22:22:50 +0000

    Set up the basic project
    Added a little maze scene and a character for testing the tools.

---
+++ GAM250/Assets/Game/Movment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movment : MonoBehaviour
+{
+    public float speed = 4;
+
+    Rigidbody rBody;
+
+    private void Awake()
+    {
+        rBody = GetComponent<Rigidbody>(); //Grab Rigidbody from player object
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal") * speed; //Get the horizontal axis input and multiply it by the speed
+        float v = Input.GetAxis("Vertical") * speed;    //Get the vertical axis input and multiply it by the speed
+
+        rBody.velocity = new Vector3(h,rBody.velocity.y,v); //Set the y velocity to it's self so we don't change gravity
+    }
+}
---
+++ GAM250/Assets/LineUp/Scripts/Manager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LineUp
+{
+    public class Manager : MonoBehaviour
+    {
+        public string newName = "Test";
+
+        private string createTableURL = "http://185.52.2.95/StartNewData.php";
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartCoroutine(CreateNewTable());
+            }
+        }
+
+
+        IEnumerator CreateNewTable()
+        {
+            string post = createTableURL + "tableName=" + WWW.EscapeURL(newName);
+            WWW tablePost = new WWW(post);
+            yield return tablePost;
+
+            if (tablePost.error != null)
+                Debug.Log("Ding Table Ready");
+        }
+    }
+}
